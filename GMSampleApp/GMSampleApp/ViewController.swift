23021e36eab1ae20add88c0ebb5199e409068670
K Madeve Gowda <sheelakarthik@Ks-MacBook-Pro.local>
2021-01-23 23:22:02 -0800

    Skeleton project

---
+++ GMSampleApp/GMSampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GMSampleApp
+//
+//  Created by K Madeve Gowda on 23/01/21.
+//  Copyright © 2021 sheela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GMSampleApp/GMSampleAppTests/GMSampleAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  GMSampleAppTests.swift
+//  GMSampleAppTests
+//
+//  Created by K Madeve Gowda on 23/01/21.
+//  Copyright © 2021 sheela. All rights reserved.
+//
+
+import XCTest
+@testable import GMSampleApp
+
+class GMSampleAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
