7a97e6dcb498b9a020012871c39eb44f56028a24
K Madeve Gowda <sheelakarthik@Ks-MacBook-Pro.local>
2021-01-23 23:40:38 -0800

    Added basic Service manager class

---
+++ GMSampleApp/GMSampleApp/ServiceManager.swift
@@ -0,0 +1,18 @@
+//
+//  ServiceManager.swift
+//  GMSampleApp
+//
+//  Created by K Madeve Gowda on 23/01/21.
+//  Copyright © 2021 sheela. All rights reserved.
+//
+
+import Foundation
+
+class ServiceManager {
+
+    static let sharedManager = ServiceManager()
+
+    func fetchGitCommits(){
+        
+    }
+}
